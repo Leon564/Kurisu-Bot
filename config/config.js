59bde32cc7356cde8b1f4a9cd4335bd0073652f1
Leon564 <nleon564.nl@gmail.com>
2022-04-19 17:42:35 -0600

    feat(commands): added stickerbg command

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
 module.exports = {
-  prefix: process.env.prefix || "!", //Your bot prefix example: "!"  
+  prefix: process.env.prefix || "!", //Your bot prefix example: "!" 
+  BgApikey: process.env.BgApikey || "", //Your Bg Api key (https://api.remove.bg/v1.0/removebg) 
 };
