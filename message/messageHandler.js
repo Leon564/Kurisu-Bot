8b6cbfca0b4627a4dabc60deda384dbde7f2eb01
Leon564 <nleon564.nl@gmail.com>
2022-06-23 19:57:35 -0600

    fix(): fixing recording state in music command

--- message/messageHandler.js
+++ message/messageHandler.js
@@ -130,8 +130,8 @@ module.exports = async (m, conn) => {
 
     //youtube
     case "music":
-      const result = await commands.youtube.youtubeToMp3(kurisu);
       await kurisu.sendPresenceUpdate("recording", kurisu.from);
+      const result = await commands.youtube.youtubeToMp3(kurisu);      
       await kurisu.sendMessage(kurisu.from, result[0], result[1]);
       break;
 
@@ -291,7 +291,8 @@ module.exports = async (m, conn) => {
         }
       );
       break;
-
+      
+    //default return to default state      
     default:
       await kurisu.sendPresenceUpdate("paused", kurisu.from);
       break;
