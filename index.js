07dec6c9d593a733c64a5148c2819a06d9baafca
Leon564 <nleon564.nl@gmail.com>
2022-06-26 13:56:50 -0600

    refactor(): update baileys connection

--- index.js
+++ index.js
@@ -1,46 +1,111 @@
-const makeWASocket = require("@adiwajshing/baileys");
+const {
+  AnyMessageContent,
+  delay,
+  DisconnectReason,
+  fetchLatestBaileysVersion,
+  makeInMemoryStore,
+  MessageRetryMap,
+  useMultiFileAuthState,
+  ...makeWASocket
+} = require("@adiwajshing/baileys");
+const MAIN_LOGGER = require("@adiwajshing/baileys/lib/Utils/logger").default;
 const messageHandler = require("./message");
 
-const { state, saveState } = makeWASocket.useSingleFileAuthState(
-  "./config/auth_info_multi.json"
-);
+const logger = MAIN_LOGGER.child({});
+logger.level = "trace";
 
-async function connectToWhatsApp() {
+const useStore = !process.argv.includes("--no-store");
+const doReplies = !process.argv.includes("--no-reply");
+
+// external map to store retry counts of messages when decryption/encryption fails
+// keep this out of the socket itself, so as to prevent a message decryption/encryption loop across socket restarts
+//const msgRetryCounterMap: MessageRetryMap = { }
+const msgRetryCounterMap = {};
+
+// the store maintains the data of the WA connection in memory
+// can be written out to a file & read from it
+const store = useStore ? makeInMemoryStore({ logger }) : undefined;
+store?.readFromFile("./config/baileys_store_multi.json");
+// save every 10s
+setInterval(() => {
+  store?.writeToFile("./config/baileys_store_multi.json");
+}, 10_000);
+
+// start a connection
+const startSock = async () => {
+  const { state, saveCreds } = await useMultiFileAuthState(
+    "./config/auth_info_baileys"
+  );
+  // fetch latest version of WA Web
+  const { version, isLatest } = await fetchLatestBaileysVersion();
+  console.log(`using WA v${version.join(".")}, isLatest: ${isLatest}`);
+
   const sock = makeWASocket.default({
+    version,
+    logger,
     printQRInTerminal: true,
-    auth: state,    
+    auth: state,
+    msgRetryCounterMap,
+    // implement to handle retries
+    getMessage: async (key) => {
+      return {
+        conversation: "hello",
+      };
+    },
   });
 
-  sock.ev.on("creds.update", saveState);
+  store?.bind(sock.ev);
+
+  sock.ev.on("call", (item) => console.log("recv call event", item));
+  sock.ev.on("chats.set", (item) =>
+    console.log(`recv ${item.chats.length} chats (is latest: ${item.isLatest})`)
+  );
+  sock.ev.on("messages.set", (item) =>
+    console.log(
+      `recv ${item.messages.length} messages (is latest: ${item.isLatest})`
+    )
+  );
+  sock.ev.on("contacts.set", (item) =>
+    console.log(`recv ${item.contacts.length} contacts`)
+  );
+
+  sock.ev.on("messages.upsert", async (m) => {
+    console.log(JSON.stringify(m, undefined, 2));
+  
+    const msg = m.messages[0];
+    if (m.type === "notify" && doReplies && msg.message) {
+      //MessageHandler here
+      messageHandler(msg, sock);
+    }
+  });
+
+  sock.ev.on("messages.update", (m) => console.log(m));
+  sock.ev.on("message-receipt.update", (m) => console.log(m));
+  sock.ev.on("presence.update", (m) => console.log(m));
+  sock.ev.on("chats.update", (m) => console.log(m));
+  sock.ev.on("chats.delete", (m) => console.log(m));
+  sock.ev.on("contacts.upsert", (m) => console.log(m));
 
   sock.ev.on("connection.update", (update) => {
     const { connection, lastDisconnect } = update;
     if (connection === "close") {
-      const shouldReconnect =
-        lastDisconnect.error?.output?.statusCode !==
-        makeWASocket.DisconnectReason.loggedOut;
-      console.log(
-        "connection closed due to ",
-        lastDisconnect.error,
-        ", reconnecting ",
-        shouldReconnect
-      );
       // reconnect if not logged out
-      if (shouldReconnect) {
-        connectToWhatsApp();
+      if (
+        lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
+      ) {
+        startSock();
+      } else {
+        console.log("Connection closed. You are logged out.");
       }
-    } else if (connection === "open") {
-      console.log("opened connection");
     }
-  });
 
-  sock.ev.on("messages.upsert", async ({ messages }) => {
-    const m = messages[0];
-    if (!m.message) return;
-    messageHandler(m, sock);
+    console.log("connection update", update);
   });
-  
+  // listen for when the auth credentials is updated
+  sock.ev.on("creds.update", saveCreds);
 
-}
+  return sock;
+};
+
 // run in main file
-connectToWhatsApp();
+startSock();
