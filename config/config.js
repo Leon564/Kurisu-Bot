f4d8b1f443c884adc0b42e3d08d3a423a065a158
Leon564 <nleon564.nl@gmail.com>
2022-07-06 18:04:22 -0600

    feat(): show the version of the bot in the message templates

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+const { version } = require("../package.json");
 module.exports = {
-  prefix: process.env.prefix || "!", //Your bot prefix example: "!" 
-  BgApikey: process.env.BgApikey || "", //Your Bg Api key (https://api.remove.bg/v1.0/removebg) 
+  prefix: process.env.prefix || "!", //Your bot prefix example: "!"
+  BgApikey: process.env.BgApikey || "", //Your Bg Api key (https://api.remove.bg/v1.0/removebg)
+  version,
 };
