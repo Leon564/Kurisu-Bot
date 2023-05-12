08c24bbffe11162d1afa09c7019f3dd1ff43f856
Leon564 <nleon564.nl@gmail.com>
2023-05-12 15:08:03 -0600

    update

--- src/lib/useCases/music.useCase.ts
+++ src/lib/useCases/music.useCase.ts
@@ -8,26 +8,35 @@ const music = async ({ data, utils }: UseCaseParams): Promise<SendData> => {
   try {
     if (!data.message.outCommandMessage)
       return { type: 'text', text: 'No se ha solicitado ninguna canción' }
-    const song = await mediaTube.get({ query: data.message.outCommandMessage! }).search()
+    const song = await mediaTube
+      .get({ query: data.message.outCommandMessage! })
+      .search()
     const tempFilename = `${Date.now()}-${song.title}.mp3`
     mp3Downloader.download(song.id, tempFilename)
     console.log('userId', data.userId)
     return new Promise((resolve, reject) => {
-      mp3Downloader.on('progress', function (progress) {
-      })
-      mp3Downloader.on('finished', async (err, result) => {
+      mp3Downloader.on('progress', function (progress: any) {})
+      mp3Downloader.on('finished', async (err: any, result: any) => {
         if (err) return reject(err)
 
         if (result.file.includes(tempFilename)) {
           const buffer = fs.readFileSync(result.file)
           fs.rmSync(result.file, { recursive: true })
           //fs.unlinkSync(data.file)
-         
-          utils.sendReply({type: 'audio',userId: data.userId , media: buffer, fakeQuoted: song.title, quoted: true, reacttion: '🎵', ptt:false, socket:data.socket})
+
+          utils.sendReply({
+            type: 'audio',
+            userId: data.userId,
+            media: buffer,
+            fakeQuoted: song.title,
+            quoted: true,
+            reacttion: '🎵',
+            ptt: false,
+            socket: data.socket
+          })
         }
       })
     })
-
   } catch (e) {
     console.log(e)
     return { type: 'text', text: 'Error al descargar la canción' }
