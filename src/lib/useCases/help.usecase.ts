6ec15690cc58937fcd7e2073b1d0e9a9d4e190b3
Leon564 <nleon564.nl@gmail.com>
2023-05-12 14:38:46 -0600

    update

--- src/lib/useCases/help.usecase.ts
+++ src/lib/useCases/help.usecase.ts
@@ -5,7 +5,7 @@ const help = ({ data, utils }: UseCaseParams): SendData => {
     type: 'text',
     text: '¡Hola! Soy Kurisu-Bot.\nActualmente, solo están disponibles los siguientes comandos:\n\n- !help\n- !sticker\n- !music\n- !img\n- !gif\n\nLamentablemente, los demás comandos no están funcionando temporalmente.',
     quoted: true,
-    reacttion: '🎲'
+    reacttion: '🤖'
   }
 }
 
