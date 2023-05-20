18dbcada0114f02f0e13ed3597fb04c52bfb98fa
Leon564 <nleon564.nl@gmail.com>
2023-05-19 22:10:40 -0600

    fix(): empty media sticker

--- src/lib/useCases/sticker.useCase.ts
+++ src/lib/useCases/sticker.useCase.ts
@@ -4,7 +4,8 @@ import { SendData, UseCaseParams } from '../../shared/interfaces/types'
 
 const sticker = async ({data,utils}: UseCaseParams): Promise<SendData> => {
   const media = await data.message.downloadMedia()
-  const stk = await new Sticker(media!).toBuffer()
+  if (!media) return { type: 'text', text: 'Se neceita una imagen o video para crear el sticker.', quoted: true, reacttion: '❌' }
+  const stk = await new Sticker(media).toBuffer()
   return {
     type: 'sticker',
     media: stk,
