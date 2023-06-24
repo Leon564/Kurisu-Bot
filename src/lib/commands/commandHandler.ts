1c361171cd0dbb29ec9211af3c2beb56b5da7795
Leon564 <nleon564.nl@gmail.com>
2023-06-24 14:56:06 -0600

    refactor(): command handler

--- src/lib/commands/commandHandler.ts
+++ src/lib/commands/commandHandler.ts
@@ -1,91 +1,80 @@
-import { proto } from '@whiskeysockets/baileys'
+import { proto } from "@whiskeysockets/baileys";
 import {
   BaileysSocket,
   MessageData,
-  SendData,
-  Utils
-} from '../../shared/interfaces/types'
-import { MessageMapper } from '../../shared/infrastructure/baileys/baileys.mapper'
-import { commands } from './commands'
-import { readFileSync } from 'fs'
+  SendData
+} from "../../shared/interfaces/types";
+import { MessageMapper } from "../../shared/infrastructure/baileys/baileys.mapper";
+import { commands } from "./commands";
 
 class commandHandler {
   constructor(
     private socket: BaileysSocket,
     private message: proto.IWebMessageInfo,
     private messageData: MessageData
-  ) { }
+  ) {}
 
   static async start(socket: BaileysSocket, message: proto.IWebMessageInfo) {
-    //console.log('message', message.message?.chat?.displayName)
-    const messageData = MessageMapper.toDomain({ data: message, socket })
-    console.log('messageData', messageData.message.command)
-    const handler = new commandHandler(socket, message, messageData)
-    return handler.messageHandler()
+    const messageData = MessageMapper.toDomain({ data: message, socket });
+    if (!messageData.message.command) return;
+    const handler = new commandHandler(socket, message, messageData);
+    return handler.messageHandler();
   }
 
   async messageHandler() {
-    const { messageData, socket, message } = this
+    const { messageData, socket, message } = this;
     if (messageData.message.isCommand) {
-      await socket.sendPresenceUpdate('composing', messageData.userId)
-      const reply = await commands.execute(messageData)
-      if (reply) await this.sendReply(reply)
+      await socket.sendPresenceUpdate("composing", messageData.userId);
+      const reply = await commands.execute(messageData);
+      if (reply) await this.sendReply(reply);
     }
   }
 
   async sendReply(data: SendData) {
-    const { userId, device, socket } = this.messageData
+    const { userId, device, socket } = this.messageData;
 
     //quoting
-    let quoted: any = data.quoted ? { quoted: this.message } : {}
+    let quoted: any = data.quoted ? { quoted: this.message } : {};
     if (data.fakeQuoted) {
       quoted = MessageMapper.replyFakeMessage({
-        text: data.fakeQuoted || 'hola',
+        text: data.fakeQuoted || "hola",
         userId: socket.user!.id.includes(":")
           ? socket.user!.id.split(":")[0] + "@s.whatsapp.net"
-          : socket.user!.id
-      })
+          : socket.user!.id,
+      });
     }
 
-    const messageContent = MessageMapper.toSocket(data, device)
-    console.log(data.userId)
-    console.log({ userId, usd: data.userId })
+    const messageContent = MessageMapper.toSocket(data, device);
     await this.socket.sendMessage(data.userId || userId, messageContent, {
-      ...quoted
-    })
-    //await this.socket.sendMessage(userId, {image:{url:'https://ezgif.com/save/ezgif-2-b94f67342f.gif'}}, { ...quoted })
+      ...quoted,
+    });
 
     if (data.reacttion)
       await this.socket.sendMessage(userId, {
-        react: { text: data.reacttion, key: this.message.key }
-      })
+        react: { text: data.reacttion, key: this.message.key },
+      });
   }
 
   async sendDReply(data: SendData) {
-    //const { userId, device } = this.messageData
-
-    //quoting
-    let quoted: {} = {}
+    let quoted: {} = {};
     if (data.fakeQuoted) {
       quoted = MessageMapper.replyFakeMessage({
-        text: data.fakeQuoted || 'hola'
-      })
+        text: data.fakeQuoted || "hola",
+      });
     }
 
     const messageContent = MessageMapper.toSocket(
       data,
-      data.device || 'Android'
-    )
-    console.log(data.userId)
-    //console.log({userId, usd: data.userId})
-    await data.socket!.sendMessage(data.userId!, messageContent, { ...quoted })
-    //await this.socket.sendMessage(userId, {image:{url:'https://ezgif.com/save/ezgif-2-b94f67342f.gif'}}, { ...quoted })
+      data.device || "Android"
+    );
+
+    await data.socket!.sendMessage(data.userId!, messageContent, { ...quoted });
 
     if (data.reacttion && data.messageReactKey)
       await data.socket!.sendMessage(data.userId!, {
-        react: { text: data.reacttion, key: data.messageReactKey as any }
-      })
+        react: { text: data.reacttion, key: data.messageReactKey as any },
+      });
   }
 }
 
-export default commandHandler
+export default commandHandler;
