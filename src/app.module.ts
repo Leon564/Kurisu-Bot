bfa666cc9584b4dd855ce78690f49b25df00fad7
Leon564 <nleon564.nl@gmail.com>
2023-07-03 00:27:51 -0600

    feat(app): remove media service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { AppController } from './application/controllers/app.controller';
 import { ChatService } from './application/services/chat.service';
 import { MessageCommandService } from './application/services/message-command.service';
 import { FirebaseService } from './application/services/firebase.service';
-import { MediaService } from './application/services/media.service';
 import { appConfig } from './configs/app.config';
 import { firebaseConfig } from './configs/firebase.config';
 import { GroupParticipantsCommandService } from './application/services/group-participants-command.service';
 import { StickerService } from './application/services/sticker.service';
+import { RemoveBgService } from './application/services/remove-bg.service';
 
 @Module({
   controllers: [AppController],
@@ -18,8 +18,8 @@ import { StickerService } from './application/services/sticker.service';
     GroupParticipantsCommandService,
     FirebaseService,
     ChatService,
-    MediaService,
     StickerService,
+    RemoveBgService,
   ],
 })
 export class AppModule {}
